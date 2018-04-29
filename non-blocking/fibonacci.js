fdff64b45e8ea1cd80f405b1886c0f37ed29a589
Cedric <cedpoilly@gmail.com>
2018-04-29 10:56:26 +0400

    Add files via upload

---
+++ non-blocking/fibonacci.js
@@ -0,0 +1,13 @@
+const INPUT = 40
+
+function fibonacci(num) {
+  if (num <= 1) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+function calculateFibonacci (origin) {
+  console.time(`${INPUT} --- ${origin}`)
+  const result = fibonacci(INPUT)
+  console.timeEnd(`${INPUT} --- ${origin}`)
+  return result
+}
