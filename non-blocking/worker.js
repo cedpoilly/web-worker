0fd994c8a67579f1b7cfece4f46cac6e0f9a6303
Cedrik1712 <cedricpoilly@outlook.com>
2018-05-16 13:44:46 +0400

    Performance readme.md

--- non-blocking/worker.js
+++ non-blocking/worker.js
@@ -5,6 +5,13 @@ addEventListener('message', onmessage)
 function onmessage(event) {
   const INPUT = event.data
   const result = calculateFibonacci(INPUT, 'WORKER')
+  postMessage(result)
+}
+
+
+function onmessage(event) {
+  const INPUT = event.data
+  const result = calculateFibonacci(INPUT, 'WORKER')
   // console.log('Execution is synchronous but does not block the main thread.')
   postMessage(result)
 }
---
+++ performance/readme.md
@@ -0,0 +1,11 @@
+# Performance Demo
+## Initialisation Flow
+1. Instantiate UI
+2. Split data, load workers
+3. Initial search: '' (empty string)
+
+## Demo Instructions
+1. Filter by worker: true; Expensive Calculation: false
+2. Filter by worker: true; Expensive Calculation: true
+3. Filter by worker: false; Expensive Calculation: false
+4. Filter by worker: false; Expensive Calculation: true
